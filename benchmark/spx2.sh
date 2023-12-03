6f1f6ff91e0c7c624b0b400939aa125ed341efb7
Andreas Raeder <andreas.raeder@isc.fraunhofer.de>
2023-12-03 11:07:24 +0000

    add exectime

--- benchmark/spx2.sh
+++ benchmark/spx2.sh
@@ -45,6 +45,7 @@ echo_tee() {
 # Provide realpath, basedir and set as cwd
 real_path=$(realpath "$0")
 basedir=$(dirname "$real_path")
+exectime_sh=$basedir/exectime.sh
 cd "$basedir" || exit 0
 cwd=$(pwd)
 ###########################################
@@ -66,8 +67,9 @@ mkdir -p "$cwd"/{data,measurement}/"$tgt_name"
 # echo "$jsonpointer"
 
 # Create measurement csv
+measurement_start=$(get_ts)
 echo "Process, Parameter, Value" | tee "$measurement_file"
-echo_tee "MEASUREMENT, START, $(get_ts)" 
+echo_tee "MEASUREMENT, START, $measurement_start" 
 echo_tee "MEASUREMENT, SCRIPT, $real_path"
 echo_tee "MEASUREMENT, BASEDIR, $basedir"
 echo_tee "MEASUREMENT, CWD, $cwd"
@@ -81,7 +83,8 @@ echo_tee "MEASUREMENT, PID, $$"
 # Output: spX.n3
 sp2b=$cwd/sp2b/bin
 sp2b_txt=$cwd/measurement/"$tgt_name"/sp2b.txt
-echo_tee "SP2B, START, $(get_ts)"
+sp2b_start=$(get_ts)
+echo_tee "SP2B, START, $sp2b_start"
 echo_tee "SP2B, DIR, $sp2b"
 
 # Change to sp2b dir
@@ -96,21 +99,25 @@ mv "$sp2b"/sp2b.n3 "$data_dir"/"$tgt_name".n3
 sp2b_triples=$(grep -o 'total triples=[0-9]*$' "$sp2b_txt" | tail -n 1 | cut -d '=' -f 2)
 sp2b_filesize=$(grep -o 'total file size=[0-9]*KB$' "$sp2b_txt"| tail -n 1 | cut -d '=' -f 2)
 # Output sp2b results
+sp2b_end=$(get_ts)
 echo_tee "SP2B, TRIPLES, $sp2b_triples"
 echo_tee "SP2B, FILESIZE, $sp2b_filesize"
-echo_tee "SP2B, END, $(get_ts)"
+echo_tee "SP2B, END, $sp2b_end"
+$exectime_sh "SP2B" "$sp2b_start" "$sp2b_end"
 ### SP2B END ###
 
 
 ### RDF2RAPSQL START ###
 # Input: spX.n3
 # Target: rapsql database
-echo_tee "RDF2RAPSQL, START, $(get_ts)"
+rdf2rapsql_start=$(get_ts)
+echo_tee "RDF2RAPSQL, START, $rdf2rapsql_start"
 # Run rdf2rapsql
 rdf2rapsql=$(realpath "$cwd/rdf2rapsql/rdf2rapsql2.sh")
 "$rdf2rapsql" -t "$triples" | tee -a "$measurement_file" || exit 1
-echo_tee "RDF2RAPSQL, END, $(get_ts)"
-
+rdf2rapsql_end=$(get_ts)
+echo_tee "RDF2RAPSQL, END, $rdf2rapsql_end"
+$exectime_sh "RDF2RAPSQL" "$rdf2rapsql_start" "$rdf2rapsql_end"
 ### RDF2RAPSQL END ###
 
 
@@ -122,4 +129,6 @@ echo_tee "RDF2RAPSQL, END, $(get_ts)"
 ### RAPSQL_SPARQL END ###
 
 ### MEASUREMENT END ###
-echo_tee "MEASUREMENT, END, $(get_ts)"
+measurement_end=$(get_ts)
+echo_tee "MEASUREMENT, END, $measurement_end"
+$exectime_sh "MEASUREMENT" "$measurement_start" "$measurement_end"
