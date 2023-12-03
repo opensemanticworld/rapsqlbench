59d0f139268eefe23e3db69d64f3efae1ae78449
Andreas Raeder <andreas.raeder@isc.fraunhofer.de>
2023-12-03 11:17:10 +0000

    clean up

--- benchmark/rdf2rapsql/writesql.sh
+++ benchmark/rdf2rapsql/writesql.sh
@@ -1,37 +1,27 @@
 #!/bin/bash
 
 # Input parameters
-graph_name="$1"
-resource_file="$2"
-literal_file="$3"
-blanknode_file="$4"
-datatypeproperty_file="$5"
-objectproperty_file="$6"
+graph_name=$1
+rdf2pg_nres=$2
+rdf2pg_nlit=$3
+rdf2pg_nbn=$4
 
-# ensure file paths
-if [ ! -f "$resource_file" ]; then
-  echo "File not found: $resource_file"
-  exit 1
-fi
-if [ ! -f "$literal_file" ]; then
-  echo "File not found: $literal_file"
-  exit 1
-fi
-if [ ! -f "$blanknode_file" ]; then
-  echo "File not found: $blanknode_file"
-  exit 1
-fi
-if [ ! -f "$datatypeproperty_file" ]; then
-  echo "File not found: $datatypeproperty_file"
-  exit 1
-fi
-if [ ! -f "$objectproperty_file" ]; then
-  echo "File not found: $objectproperty_file"
-  exit 1
-fi
 
-sql="
-SELECT now() AS \"IMPORT CONFIG\";
+raw_file_dir=$(dirname "$rdf2pg_nres")
+init_sql="$raw_file_dir"/import/init.sql
+sql_dir="$raw_file_dir"/import/nodes
+nres_sql=$sql_dir/$(basename "$rdf2pg_nres" .csv).sql
+nlit_sql=$sql_dir/$(basename "$rdf2pg_nlit" .csv).sql
+nbn_sql=$sql_dir/$(basename "$rdf2pg_nbn" .csv).sql
+mkdir -p "$sql_dir"
+
+# Create initial sql file
+
+sql_create_basefile() {
+  local sql_file="$1"
+  local keyword="$2"
+  # Create base file
+  echo "--import/$keyword.sql
 
 -- age config
 LOAD 'age';
@@ -40,64 +30,108 @@ SET search_path TO ag_catalog;
 -- disable notices https://stackoverflow.com/a/3531274
 SET client_min_messages TO WARNING;
 
--- create graph
-SELECT create_graph('$graph_name');
+SELECT now() AS \"START DBIMPORT $keyword\";
+" > "$sql_file"
+}
 
+sql_create_graph() {
+  local sql_file="$1"
+  local graph_name="$2"
+  # Create graph
+  echo "SELECT create_graph('$graph_name');" >> "$sql_file"
+}
 
--- create vlabels (vertices = nodes)
-SELECT create_vlabel('$graph_name','Resource');
-SELECT create_vlabel('$graph_name','Literal');
-SELECT create_vlabel('$graph_name','BlankNode');
+# Create SQL statement functions
+sql_create_vlabel() {
+  local sql_file="$1"
+  local graph_name="$2"
+  local vlabel="$3"
+  # Create vlabel
+  echo "SELECT create_vlabel('$graph_name','$vlabel');" >> "$sql_file"
+}
 
-
--- create elabels
-SELECT create_elabel('$graph_name','DatatypeProperty');
-SELECT create_elabel('$graph_name','ObjectProperty');
-
-
--- import nodes (important -> first nodes)
-SELECT now() AS \"IMPORT START\";
+sql_load_edges_from_file() {
+  local sql_file="$1"
+  local graph_name="$2"
+  local vlabel="$3"
+  local csv_path="$4"
+  # Append load labels from file statement
+  echo "
 SELECT load_labels_from_file(
   '$graph_name',
-  'Resource',
-  '$resource_file',
+  '$vlabel',
+  '$csv_path',
   true
 );
-SELECT now() AS \"IMPORT RESOURCE\";
 
-SELECT load_labels_from_file(
-  '$graph_name',
-  'Literal',
-  '$literal_file',
-  true
-);
-SELECT now() AS \"IMPORT LITERAL\";
+SELECT now() AS \"END DBIMPORT $vlabel\";" >> "$sql_file"
+}
 
-SELECT load_labels_from_file(
-  '$graph_name',
-  'BlankNode',
-  '$blanknode_file',
-  true
-);
-SELECT now() AS \"IMPORT BLANKNODE\";
+# Create sql files
+sql_create_basefile "$init_sql" "$graph_name"
+sql_create_basefile "$nres_sql" "Resource"
+sql_create_basefile "$nlit_sql" "Literal"
+sql_create_basefile "$nbn_sql" "BlankNode"
+
+# Append statements to sql files
+sql_create_graph "$init_sql" "$graph_name"
+sql_create_vlabel "$nres_sql" "$graph_name" "Resource"
+sql_create_vlabel "$nlit_sql" "$graph_name" "Literal"
+sql_create_vlabel "$nbn_sql" "$graph_name" "BlankNode"
+sql_load_edges_from_file "$nres_sql" "$graph_name" "Resource" "$rdf2pg_nres"
+sql_load_edges_from_file "$nlit_sql" "$graph_name" "Literal" "$rdf2pg_nlit"
+sql_load_edges_from_file "$nbn_sql" "$graph_name" "BlankNode" "$rdf2pg_nbn"
 
 
--- import edges
-SELECT load_edges_from_file(
-  '$graph_name',
-  'DatatypeProperty',
-  '$datatypeproperty_file'
-);
-SELECT now() AS \"IMPORT DATATYPEPROPERTY\";
+# # Basic Import and node import statements
+# sql="
+# SELECT now() AS \"IMPORT CONFIG\";
 
-SELECT load_edges_from_file(
-  '$graph_name',
-  'ObjectProperty',
-  '$objectproperty_file'
-);
-SELECT now() AS \"IMPORT OBJECTPROPERTY\";
+# -- age config
+# LOAD 'age';
+# SET search_path TO ag_catalog;
 
-SELECT now() AS \"IMPORT END\";
-"
-echo "$sql" > import.sql
-# echo "$sql" > "$graph_name".sql
+# -- disable notices https://stackoverflow.com/a/3531274
+# SET client_min_messages TO WARNING;
+
+# -- create graph
+# SELECT create_graph('$graph_name');
+
+
+# -- create vlabels (vertices = nodes)
+# SELECT create_vlabel('$graph_name','Resource');
+# SELECT create_vlabel('$graph_name','Literal');
+# SELECT create_vlabel('$graph_name','BlankNode');
+
+
+# -- import nodes (important -> first nodes)
+# SELECT now() AS \"IMPORT START\";
+# SELECT load_labels_from_file(
+#   '$graph_name',
+#   'Resource',
+#   '$rdf2pg_nres',
+#   true
+# );
+# SELECT now() AS \"IMPORT RESOURCE\";
+
+# SELECT load_labels_from_file(
+#   '$graph_name',
+#   'Literal',
+#   '$rdf2pg_nlit',
+#   true
+# );
+# SELECT now() AS \"IMPORT LITERAL\";
+
+# SELECT load_labels_from_file(
+#   '$graph_name',
+#   'BlankNode',
+#   '$rdf2pg_nbn',
+#   true
+# );
+# SELECT now() AS \"IMPORT BLANKNODE\";
+
+
+# "
+
+# Create SQL import file
+# echo "$sql" >> import.sql
--- benchmark/spx.sh
+++ benchmark/spx.sh
@@ -3,31 +3,10 @@
 # License:  Apache License 2.0
 # Usage:    /bin/bash <PATH_TO_rapsqlbench.sh" <POSITIVE_INTEGER>
 
-# Positive input integer to set the number of triples
-input=$1
-
-# Function to check if a value is a positive integer
-is_positive_integer() {
-  if [[ $1 =~ ^[1-9][0-9]*$ ]]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
-# Check if input exists
-if [[ -z "$input" ]]; then
-  echo "Error: No input provided. Please provide a positive integer." >&2
-  exit 1
-else 
-  # Check if input is a positive integer
-  if is_positive_integer "$input"; then
-    triples=$input
-  else
-    echo "Error: Invalid input. Please provide a positive integer." >&2
-    exit 1
-  fi
-fi
+graphname=$1
+triples=$2
+memory=$3
+cores=$4
 
 # Function to get the current timestamp
 get_ts() {
@@ -45,18 +24,18 @@ echo_tee() {
 # Provide realpath, basedir and set as cwd
 real_path=$(realpath "$0")
 basedir=$(dirname "$real_path")
+exectime_sh=$basedir/exectime.sh
 cd "$basedir" || exit 0
 cwd=$(pwd)
 ###########################################
 
 ### MEASUREMENT START ###
 # Create project dirs
-tgt_name=sp"$triples"
-data_dir=$cwd/data/"$tgt_name"
-measurement_dir=$cwd/measurement/"$tgt_name"
+data_dir=$cwd/data/"$graphname"
+measurement_dir=$cwd/measurement/"$graphname"
 measurement_file="$measurement_dir"/measurement.csv
 # Create data and measurement dirs by triples
-mkdir -p "$cwd"/{data,measurement}/"$tgt_name"
+mkdir -p "$cwd"/{data,measurement}/"$graphname"
 
 # # Create measurement json from schema
 # schema_json="$basedir"/schema.json
@@ -66,8 +45,9 @@ mkdir -p "$cwd"/{data,measurement}/"$tgt_name"
 # echo "$jsonpointer"
 
 # Create measurement csv
+measurement_start=$(get_ts)
 echo "Process, Parameter, Value" | tee "$measurement_file"
-echo_tee "MEASUREMENT, START, $(get_ts)" 
+echo_tee "MEASUREMENT, START, $measurement_start" 
 echo_tee "MEASUREMENT, SCRIPT, $real_path"
 echo_tee "MEASUREMENT, BASEDIR, $basedir"
 echo_tee "MEASUREMENT, CWD, $cwd"
@@ -80,8 +60,9 @@ echo_tee "MEASUREMENT, PID, $$"
 # Generate "-t X" rdf triples 
 # Output: spX.n3
 sp2b=$cwd/sp2b/bin
-sp2b_txt=$cwd/measurement/"$tgt_name"/sp2b.txt
-echo_tee "SP2B, START, $(get_ts)"
+sp2b_txt=$cwd/measurement/"$graphname"/sp2b.txt
+sp2b_start=$(get_ts)
+echo_tee "SP2B, START, $sp2b_start"
 echo_tee "SP2B, DIR, $sp2b"
 
 # Change to sp2b dir
@@ -91,26 +72,30 @@ cd "$sp2b" || exit
 # Change back to cwd
 cd "$cwd" || exit
 # Move sp2b data to data dir
-mv "$sp2b"/sp2b.n3 "$data_dir"/"$tgt_name".n3
+mv "$sp2b"/sp2b.n3 "$data_dir"/"$graphname".n3
 # Read real triple count and file size from sp2b.txt tail
 sp2b_triples=$(grep -o 'total triples=[0-9]*$' "$sp2b_txt" | tail -n 1 | cut -d '=' -f 2)
 sp2b_filesize=$(grep -o 'total file size=[0-9]*KB$' "$sp2b_txt"| tail -n 1 | cut -d '=' -f 2)
 # Output sp2b results
+sp2b_end=$(get_ts)
 echo_tee "SP2B, TRIPLES, $sp2b_triples"
 echo_tee "SP2B, FILESIZE, $sp2b_filesize"
-echo_tee "SP2B, END, $(get_ts)"
+echo_tee "SP2B, END, $sp2b_end"
+$exectime_sh "SP2B" "$sp2b_start" "$sp2b_end"
 ### SP2B END ###
 
 
 ### RDF2RAPSQL START ###
 # Input: spX.n3
 # Target: rapsql database
-echo_tee "RDF2RAPSQL, START, $(get_ts)"
+rdf2rapsql_start=$(get_ts)
+echo_tee "RDF2RAPSQL, START, $rdf2rapsql_start"
 # Run rdf2rapsql
-rdf2rapsql=$(realpath "$cwd/rdf2rapsql/rdf2rapsql.sh")
-"$rdf2rapsql" -t "$triples" | tee -a "$measurement_file" || exit 1
-echo_tee "RDF2RAPSQL, END, $(get_ts)"
-
+rdf2rapsql=$(realpath "$cwd/rdf2rapsql/rdf2rapsql3.sh")
+"$rdf2rapsql" "$graphname" "$memory" "$cores" | tee -a "$measurement_file" || exit 1
+rdf2rapsql_end=$(get_ts)
+echo_tee "RDF2RAPSQL, END, $rdf2rapsql_end"
+echo_tee "$("$exectime_sh" "RDF2RAPSQL" "$rdf2rapsql_start" "$rdf2rapsql_end")"
 ### RDF2RAPSQL END ###
 
 
@@ -122,4 +107,6 @@ echo_tee "RDF2RAPSQL, END, $(get_ts)"
 ### RAPSQL_SPARQL END ###
 
 ### MEASUREMENT END ###
-echo_tee "MEASUREMENT, END, $(get_ts)"
+measurement_end=$(get_ts)
+echo_tee "MEASUREMENT, END, $measurement_end"
+echo_tee "$("$exectime_sh" "MEASUREMENT" "$measurement_start" "$measurement_end")"
