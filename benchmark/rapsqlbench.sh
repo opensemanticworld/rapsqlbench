764231e894730c044e9b57c53e8a40f6bd7a5201
Andreas Raeder <andreas.raeder@isc.fraunhofer.de>
2024-01-23 12:05:49 +0000

    add model and transpiler option check on entrypoint

--- benchmark/rapsqlbench.sh
+++ benchmark/rapsqlbench.sh
@@ -10,7 +10,8 @@ match_graphname_pattern() {
     echo "ERROR: $graphname contains special characters or begins with digits."
     exit 1
   fi
-  # # Grammer graph name
+}
+  # # Grammer graph name (experimental)
   # # 1 names beginning with pg_ or ag_ are reserved for system schemas
   # # 2 no white spaces
   # # 3 no special characters
@@ -30,6 +31,43 @@ match_graphname_pattern() {
   # else
   #   echo "YPG2RAPSQL, GRAPH, $graph_name"
   # fi
+
+
+# Function to check if rdf2pg model option is valid
+match_model_option() {
+  local model=$1
+  if ! [[ $model =~ ^(yars|rdfid)$ ]]; then
+    echo "ERROR: Invalid model arg, must be either yars or rdfid."
+    exit 1
+  fi
+}
+
+# Function to check if rapsqltranspiler option is valid 
+match_transpiler_option() {
+  local transpiler=$1
+  if ! [[ $transpiler =~ ^(yplain|ycpo1|ycpo2|ycpo3|ymano|rplain|rcpo1|rcpo2|rcpo3|rmano)$ ]]; then
+    echo "ERROR: Invalid transpiler arg, must be either Xplain, Xcpo1, Xcpo2, Xcpo3, Xmano with X = y or r."
+    exit 1
+  fi
+}
+
+# Function to check if model and transpiler combination is valid 
+# yars:   yplain | ycpo1 (=L2L) | ycpo2 (=L2R) | ycpo3 (=L2L+L2R) | ymano
+# rdfid:  rplain | rcpo1 (=L2L) | rcpo2 (=L2R) | rcpo3 (=L2L+L2R) | rmano
+check_model_transpiler_comination () {
+  local model=$1
+  local transpiler=$2
+  if [[ $model == "yars" ]]; then
+    if ! [[ $transpiler =~ ^(yplain|ycpo1|ycpo2|ycpo3|ymano)$ ]]; then
+      echo "ERROR: Invalid arg combination, transpiler must be either yplain, ycpo1, ycpo2, ycpo3, ymano for model yars."
+      exit 1
+    fi
+  elif [[ $model == "rdfid" ]]; then
+    if ! [[ $transpiler =~ ^(rplain|rcpo1|rcpo2|rcpo3|rmano)$ ]]; then
+      echo "ERROR: Invalid arg combination, transpiler must be either rplain, rcpo1, rcpo2, rcpo3, rmano for model rdfid."
+      exit 1
+    fi
+  fi
 }
 
 # Function to check if a value is a positive integer
@@ -63,11 +101,21 @@ check_memory_resources() {
 }
 
 # Parse command line arguments
-while getopts ":g:t:m:c:i:" opt; do
+while getopts ":g:l:r:t:m:c:i:" opt; do
   case $opt in
     g)
       if match_graphname_pattern "$OPTARG"; then
         graphname=$OPTARG
+      fi
+      ;;
+    l)
+      if match_model_option "$OPTARG"; then
+        model=$OPTARG
+      fi
+      ;;
+    r)
+      if match_transpiler_option "$OPTARG"; then
+        transpiler=$OPTARG
       fi
       ;;
     t)
@@ -107,6 +155,7 @@ if [[ -z "$graphname" ]]; then
   graphname="sp2b"
 fi
 
+
 # Check if -t is set, otherwise set default to 100
 if [[ -z "$triples" ]]; then
   echo "No -t triples input provided. Using default triples 100."
@@ -136,6 +185,9 @@ if [[ $triples -gt 1000000000 ]]; then
   fi
 fi
 
+# Check if combination of model and transpiler is valid
+check_model_transpiler_comination "$model" "$transpiler"
+
 # Check if requested cores resources are available
 check_core_resources "$cores"
 
@@ -143,8 +195,31 @@ check_core_resources "$cores"
 check_memory_resources "$mem"
 
 ##########################################################
+# PERFORM BENCHMARK
 real_path=$(realpath "$0")
 basedir=$(dirname "$real_path")
 spx_sh=$basedir/spx.sh
 "$spx_sh" "$graphname" "$triples" "$mem" "$cores" "$iterations" || exit 1
 
+
+##########################################################
+# TEST OF ARGUMENTS
+# echo "graphname: $graphname"
+# echo "model: $model"
+# echo "transpiler: $transpiler"
+# echo "triples: $triples"
+# echo "mem: $mem"
+# echo "cores: $cores"
+# echo "iterations: $iterations"
+
+# Valid test commands
+# ./rapsqlbench.sh -g sp2b -l yars -r yplain -t 100 -m 1000 -c 1 -i 1
+# ./rapsqlbench.sh -g sp2b -l yars -r ycpo1 -t 100 -m 1000 -c 1 -i 1
+# ./rapsqlbench.sh -g sp2b -l rdfid -r rcpo2 -t 100 -m 1000 -c 1 -i 1
+# ./rapsqlbench.sh -g sp2b -l rdfid -r rmano -t 100 -m 1000 -c 1 -i 1
+
+# Invalid test commands
+# ./rapsqlbench.sh -g sp2b -l yars -r rplain -t 100 -m 1000 -c 1 -i 1
+# ./rapsqlbench.sh -g sp2b -l yarss -r yplain -t 100 -m 1000 -c 1 -i 1
+# ./rapsqlbench.sh -g sp2b -l yars -r rplains -t 100 -m 1000 -c 1 -i 1
+# ./rapsqlbench.sh -g sp2b -l rdfid -r ycpo3 -t 100 -m 1000 -c 1 -i 1
