dcf47ed9959574c9b2041b978596c1419546d1e7
Andreas Raeder <andreas.raeder@isc.fraunhofer.de>
2024-01-28 16:40:01 +0000

    example config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -58,16 +58,11 @@ variable "vm_map" {
     #   instance_type = "t2.micro"
     #   ebs_size      = 30
     # }
-    # "vm50k" = {
-    #   ami           = "ami-04e601abe3e1a910f"
-    #   instance_type = "r5.8xlarge"
-    #   ebs_size      = 100
-    # }
-    # "vm50k2" = {
-    #   ami           = "ami-04e601abe3e1a910f"
-    #   instance_type = "r5.8xlarge"
-    #   ebs_size      = 100
-    # }
+    "vm50k" = {
+      ami           = "ami-04e601abe3e1a910f"
+      instance_type = "r5.8xlarge"
+      ebs_size      = 100
+    }
     # "vm250k" = {
     #   ami           = "ami-04e601abe3e1a910f"
     #   instance_type = "r5.8xlarge"
